1c8693ce38569277926a6d8c8301863a433276c1
assumpter kimeu <assumpterkimeu16@gmail.com>
2018-02-19 16:24:03 +0300

    added file

---
+++ flow chart.cpp
@@ -0,0 +1,41 @@
+
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int a;
+    int b;
+    cout<<"enter the first number";
+    
+    cin>>a;
+    cout<<" enter the second number";
+    cin>> b;
+    if(a>b){
+        cout<<"a is greater";
+    }
+    else if(a==b){
+        cout<<"a is equal to be b";
+    }
+    else{cout<<"b is greater";
+        
+    }
+
+    return 0
+}
+
+
+
+
+
+
